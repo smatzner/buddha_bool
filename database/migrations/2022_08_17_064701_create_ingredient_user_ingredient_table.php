363adac68600cc90a6a75741fecaf151926c6f20
smatzner <stefanmatzner@gmx.at>
2022-08-17 08:49:43 +0200

    added migration for ingredient_user_ingredient table

---
+++ database/migrations/2022_08_17_064701_create_ingredient_user_ingredient_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ingredient_user_ingredient', function (Blueprint $table) {
+            $table->foreignId('ingredient_id');
+            $table->foreignId('user_ingredient_id');
+            $table->foreign('ingredient_id')->on('ingredients')->references('id')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('user_ingredient_id')->on('user_ingredients')->references('id')->onDelete('cascade')->onUpdate('cascade');
+            $table->primary(['ingredient_id', 'user_ingredient_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ingredient_user_ingredient');
+    }
+};
